f8e229dfda0630d5faf4e086b43c3945fa53bb4c
Jessica <j.j.isaacs@hotmail.co.uk>
2022-10-18 22:21:08 +0100

    add magic item filter for rarity

--- src/pages/MagicItems.tsx
+++ src/pages/MagicItems.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Card, CardHeader, CardContent, Grid, Typography } from '@mui/material'
+import { Card, CardHeader, CardContent, Grid, Typography, Box } from '@mui/material'
 import { getMagicItem, getMagicItemList } from 'services/API/apiService'
 import MagicItem from 'services/API/Enums/MagicItem'
 import { populateList } from 'services/helpers'
 import PageProps from 'pages/PageProps'
 import CenterSpinner from 'components/CenterSpinner'
+import SelectList, { ListItem } from 'components/forms/SelectList'
 
 const MagicItems = ({setTitle}: PageProps) => {
 
@@ -13,48 +14,60 @@ const MagicItems = ({setTitle}: PageProps) => {
   }, [])
 
   const [magicItems, setMagicItems] = useState<MagicItem[]>()
-  
+
+  const [rarityFilterOptions, setRarityFilterOptions] = useState<ListItem[]>()
+  const [rarityFilter, setRarityFilter] = useState<string>('')
+
   useEffect(() => {
-    populateList(getMagicItemList, getMagicItem, setMagicItems)
+    const setFilterOptions = (items: MagicItem[]) => {
+      setRarityFilterOptions(
+        [...new Set(items?.map(item => item.rarity.name))]
+        .map(rarity => { return new ListItem(rarity, rarity) })
+        .sort((x, y) => x.value.localeCompare(y.value))
+      )
+    }
+    populateList(getMagicItemList, getMagicItem, setMagicItems, setFilterOptions)
   }, [])
 
   return (
     <React.Fragment>
-      { 
-        !magicItems 
+      { !magicItems 
         ? <CenterSpinner />
-        : <Grid container spacing={5} alignItems="flex-start">
-          { magicItems?.map(data => 
-            <Grid item key={data.index} xs={12} sm={6} md={4}> {/* style={{display: 'flex'}}  */}
-              <Card>
-              <CardHeader
-                  title={data.name}
-                  subheader={data.rarity.name + ' - ' + data.equipment_category.name + (data.variant ? ' - variant' : '')}
-                  titleTypographyProps={{ 
-                    align: 'center' ,
-                    color: (theme) => theme.palette.info.contrastText 
-                  }}
-                  subheaderTypographyProps={{ 
-                    align: 'center' ,
-                    color: (theme) => theme.palette.info.contrastText 
-                  }}
-                  sx={{ 
-                    backgroundColor: (theme) => theme.palette.info.main 
-                  }}
-                />
-                <CardContent>
-                {
-                  data.desc.map((description, i) => 
-                    <Typography key={data.index + "-desc-" + i} variant="body1" color="text.primary" gutterBottom >
-                      {description}
-                    </Typography>  
-                  )
-                }
-                </CardContent>
-              </Card>
+        : <div>
+            <Box mb={3} >
+              <SelectList id="rarity-select" title="Rarity" value={rarityFilter} items={rarityFilterOptions} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setRarityFilter(e.target.value)}} />
+            </Box>
+            <Grid container spacing={5} alignItems="flex-start">
+              { magicItems?.filter(item => !rarityFilter || (item.rarity.name == rarityFilter)).map(data => 
+              <Grid item key={data.index} xs={12} sm={6} md={4}> {/* style={{display: 'flex'}}  */}
+                <Card>
+                  <CardHeader
+                    title={data.name}
+                    subheader={data.rarity.name + ' - ' + data.equipment_category.name + (data.variant ? ' - variant' : '')}
+                    titleTypographyProps={{ 
+                      align: 'center' ,
+                      color: (theme) => theme.palette.info.contrastText 
+                    }}
+                    subheaderTypographyProps={{ 
+                      align: 'center' ,
+                      color: (theme) => theme.palette.info.contrastText 
+                    }}
+                    sx={{ 
+                      backgroundColor: (theme) => theme.palette.info.main 
+                    }}
+                  />
+                  <CardContent>
+                    {data.desc.map((description, i) => 
+                      <Typography key={data.index + "-desc-" + i} variant="body1" color="text.primary" gutterBottom >
+                        {description}
+                      </Typography>  
+                    )}
+                  </CardContent>
+                </Card>
+              </Grid>
+              )}
             </Grid>
-          )}
-        </Grid>
+        </div>
       }
     </React.Fragment>
   )
--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -3,19 +3,25 @@ import ResourceList from 'services/API/Enums/ResourceList'
 export const populateList = async function <T>(
     getList: () => Promise<ResourceList | undefined>, 
     getItem: (index: string) => Promise<T | undefined>,
-    setItems: (value: React.SetStateAction<T[] | undefined>) => void
+    setItems: (value: React.SetStateAction<T[] | undefined>) => void,
+    callback?: (list: T[]) => void
 ) {
-    getList().then(data => {
+    getList().then((data: ResourceList | undefined) => {
         let list: T[] = []
         let promises = data?.results.map(resource => {
         return getItem(resource.index).then(x => {
             if(x) {
-            list.push(x)
+                list.push(x)
             }
         })
         }) || []
         Promise.all(promises).then(() =>
-        setItems(list)
+            {
+                setItems(list)
+                if(callback) {
+                    callback(list)
+                }
+            }
         )
     })
 }
